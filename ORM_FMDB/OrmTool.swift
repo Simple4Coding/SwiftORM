b0fec9a1b83d6b1217a6920db087c681bc8510a7
danychen <danycoding@gmail.com>
2015-12-23 23:45:07 +0800

    first commit

---
+++ ORM_FMDB/OrmTool.swift
@@ -0,0 +1,21 @@
+//
+//  OrmTool.swift
+//  ORM_FMDB
+//
+//  Created by DanyChen on 23/12/15.
+//  Copyright © 2015 DanyChen. All rights reserved.
+//
+
+import UIKit
+
+class OrmTool: UIWindow {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
